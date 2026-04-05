76b75d0cef45bd54588bb040c1bb7d910fbedda1
Mario Tejeda <marioandredub@gmail.com>
2026-04-05 22:53:53 +0100

    Add project files.

---
+++ oop-s2-1-mvc-79189/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace oop_s2_1_mvc_79189.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
